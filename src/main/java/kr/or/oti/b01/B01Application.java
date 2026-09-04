9b81cf7261c7469e519681ba4f42602691c45741
masungil2 <masungil2@daum.net>
2026-09-04 10:20:34 +0900

    다음의 마성일이 추가함

--- src/main/java/kr/or/oti/b01/B01Application.java
+++ src/main/java/kr/or/oti/b01/B01Application.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class B01Application {
 
 	//주석을 추가함 
+	//다음의 마성일이 추가함
 	public static void main(String[] args) {
 		SpringApplication.run(B01Application.class, args);
 	}
